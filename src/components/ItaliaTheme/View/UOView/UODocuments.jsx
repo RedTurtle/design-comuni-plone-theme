66156aa6acdc0cd5310388fb4afa3040f8d7f635
Wagner Trezub <wagner.trezub@redturtle.it>
2023-01-23 16:20:16 +0100

    feat: added new variable for the document title | added a separated title for public docs | fixed card text font type

--- src/components/ItaliaTheme/View/UOView/UODocuments.jsx
+++ src/components/ItaliaTheme/View/UOView/UODocuments.jsx
@@ -9,29 +9,36 @@ import { Row } from 'design-react-kit';
 const messages = defineMessages({
   documenti: {
     id: 'uo_documenti',
-    defaultMessage: 'Allegati',
+    defaultMessage: 'Documenti',
+  },
+  documenti_pubblici: {
+    id: 'uo_documenti_pubblici',
+    defaultMessage: 'Documenti Pubblici',
   },
 });
 
 const UODocuments = ({ content }) => {
   const intl = useIntl();
 
-  return content?.documenti_pubblici?.length > 0 ? (
-    <article id="allegati" className="it-page-section anchor-offset mt-5 mb-5">
-      <h3 id="header-allegati">{intl.formatMessage(messages.documenti)}</h3>
+  return (
+    <article id="allegati" className="it-page-section anchor-offset pt-5 mb-5">
       <Attachments
         content={content}
         folder_name={'allegati'}
         as_section={false}
+        title={intl.formatMessage(messages.documenti)}
       />
-      <Row className="card-wrapper card-teaser-wrapper documenti-pubblici">
-        {content?.documenti_pubblici?.map((dp, i) => (
-          <Attachment {...dp} download_url={dp?.['@id']} key={dp['@id']} />
-        ))}
-      </Row>
+      {content?.documenti_pubblici && (
+        <Row className="card-wrapper card-teaser-wrapper documenti-pubblici">
+          {content?.documenti_pubblici?.map((dp, i) => (
+            <>
+              <h5>{intl.formatMessage(messages.documenti_pubblici)}</h5>
+              <Attachment {...dp} download_url={dp?.['@id']} key={dp['@id']} />
+            </>
+          ))}
+        </Row>
+      )}
     </article>
-  ) : (
-    <></>
   );
 };
 
