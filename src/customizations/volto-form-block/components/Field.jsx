23e2472ff2f182d5334010d10ba524d4fefe0f94
Wagner Trezub <wagner.trezub@redturtle.it>
2023-03-10 11:20:43 +0100

    fix: removed unnecessary prop

--- src/customizations/volto-form-block/components/Field.jsx
+++ src/customizations/volto-form-block/components/Field.jsx
@@ -112,7 +112,6 @@ const Field = ({
               id={name}
               name={name}
               label={getLabel()}
-              infoText={description}
               isSearchable={true}
               onChange={(v) => {
                 onChange(name, v.value);
