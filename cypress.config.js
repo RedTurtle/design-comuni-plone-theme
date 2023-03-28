383f55969d7642714c504de169f9edbac230953e
Mauro Amico <mauro.amico@gmail.com>
2023-03-28 12:36:06 +0200

    ci: cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'design-comuni-plone-theme',
   e2e: {
     baseUrl: 'http://localhost:3000',
     excludeSpecPattern: ['*~'],
