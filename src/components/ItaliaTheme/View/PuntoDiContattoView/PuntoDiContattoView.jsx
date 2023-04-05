8ec9ccd4602fe637b5740fe2507a214195c9298e
Sabrina Bongiovanni <sabrinabongiovanni96@gmail.com>
2023-04-05 17:56:52 +0200

    fix: added condition for missing messages

--- src/components/ItaliaTheme/View/PuntoDiContattoView/PuntoDiContattoView.jsx
+++ src/components/ItaliaTheme/View/PuntoDiContattoView/PuntoDiContattoView.jsx
@@ -109,9 +109,11 @@ const PuntoDiContattoView = (props) => {
             return (
               <div className="my-2" key={i}>
                 <h6>
-                  {intl.formatMessage(messages.label, {
-                    value: intl.formatMessage(messages[pdc?.pdc_type]),
-                  })}
+                  {messages[pdc?.pdc_type] === undefined
+                    ? pdc?.pdc_type
+                    : intl.formatMessage(messages.label, {
+                        value: intl.formatMessage(messages[pdc?.pdc_type]),
+                      })}
                   <span className="ml-1">{renderPDCItemValue(pdc)}</span>
                 </h6>
               </div>
