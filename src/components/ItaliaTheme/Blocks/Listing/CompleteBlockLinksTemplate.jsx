5bebb7b89167e4424964281663a499dcc792f3fb
Wagner Trezub <wagner.trezub@redturtle.it>
2023-04-18 17:11:58 +0200

    feat: block listing and default sidebar settings

--- src/components/ItaliaTheme/Blocks/Listing/CompleteBlockLinksTemplate.jsx
+++ src/components/ItaliaTheme/Blocks/Listing/CompleteBlockLinksTemplate.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { UniversalLink } from '@plone/volto/components';
+import cx from 'classnames';
 import {
   Card,
   CardBody,
@@ -24,6 +25,7 @@ const CompleteBlockLinksTemplate = ({
   show_block_bg,
   show_description = true,
   id_lighthouse,
+  center_cards,
 }) => {
   return (
     <div className="complete-block-links-template">
@@ -35,7 +37,7 @@ const CompleteBlockLinksTemplate = ({
             </Col>
           </Row>
         )}
-        <Row className="items">
+        <Row className={cx('items', { 'center-cards': center_cards })}>
           {items.map((item, index) => {
             const image = ListingImage({ item, className: '' });
 
