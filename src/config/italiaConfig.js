60e51900626c4713babfdb52b7834481ce67adcc
Sabrina Bongiovanni <sabrinabongiovanni96@gmail.com>
2023-05-25 09:46:41 +0200

    fix: disallowed contact form

--- src/config/italiaConfig.js
+++ src/config/italiaConfig.js
@@ -249,6 +249,12 @@ export default function applyConfig(voltoConfig) {
     showTrasparenzaFields: false,
   };
 
+  //* DISALLOW FORM CONTACT
+
+  config.settings.nonContentRoutes = config.settings.nonContentRoutes.filter(
+    (route) => route !== '/contact-form',
+  );
+
   /******************************************************************************
    * VIEWS
    ******************************************************************************/
