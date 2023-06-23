1debbd4ac33da1ae20b9210a8b744957e2c3f706
Sabrina Bongiovanni <sabrinabongiovanni96@gmail.com>
2023-06-23 18:05:58 +0200

    fix: restored sideMenu with accordion

--- src/components/ItaliaTheme/View/Commons/SideMenu.jsx
+++ src/components/ItaliaTheme/View/Commons/SideMenu.jsx
@@ -132,97 +132,60 @@ const SideMenu = ({ data, content_uid }) => {
     setIsNavOpen(false);
   };
 
-  // const yCountEnd = document.querySelector('#main-content-section');
-  const yCountEnd = isClient
-    ? document.querySelector('#main-content-section')
-    : null;
-
   const progressValue = useMemo(() => {
     if (!isClient) return 0;
-    return (scrollY - yCountEnd.offsetTop) / yCountEnd.offsetHeight || 0;
+    return (
+      scrollY /
+        (document.documentElement.scrollHeight -
+          document.documentElement.clientHeight) || 0
+    );
   }, [scrollY, isClient]);
 
   return headers?.length > 0 ? (
     <div className="sticky-wrapper navbar-wrapper page-side-menu">
       <nav className="navbar it-navscroll-wrapper navbar-expand-lg">
-        <button
-          className={
-            isNavOpen
-              ? 'custom-navbar-toggler focus--mouse'
-              : 'custom-navbar-toggler'
-          }
-          type="button"
-          aria-controls="navbarNavB"
-          aria-expanded={isNavOpen ? 'true' : 'false'}
-          aria-label="Toggle navigation"
-          data-target="#navbarNavB"
-          onClick={() => {
-            setIsNavOpen(!isNavOpen);
-          }}
-        >
-          <span className="it-list"></span>
-          {intl.formatMessage(messages.index)}
-        </button>
-
-        <div
-          className={
-            isNavOpen ? 'navbar-collapsable expanded' : 'navbar-collapsable'
-          }
-          id="navbarNavB"
-          style={isNavOpen ? { display: 'block' } : { display: 'none' }}
-        >
-          <div
-            className="overlay"
-            style={isNavOpen ? { display: 'block' } : { display: 'none' }}
-          ></div>
-          <div className="close-div visually-hidden">
-            <button className="btn close-menu" type="button">
-              <span className="it-close"></span>
-              {intl.formatMessage(messages.close)}
-            </button>
-          </div>
-          <a
-            className="it-back-button"
-            href="#"
-            style={isNavOpen ? { display: 'block' } : { display: 'none' }}
-            onClick={(e) => {
-              e.preventDefault();
-              setIsNavOpen(!isNavOpen);
-            }}
-          >
-            <Icon
-              className="align-top"
-              color="primary"
-              icon="it-chevron-left"
-              style={{ ariaHidden: true }}
-              size="sm"
-            />
-            <span>{intl.formatMessage(messages.back)}</span>
-          </a>
-          <div className="menu-wrapper">
-            <div className="link-list-wrapper menu-link-list">
-              <h3>{intl.formatMessage(messages.index)}</h3>
-              <div className="mb-3">
-                <Progress
-                  value={progressValue > 0 ? 100 * progressValue : 0}
-                  role="progressbar"
-                />
-              </div>
-              <ul className="link-list" data-element="page-index">
-                {headers.map((item, i) => (
-                  <li className="nav-item" key={item.id}>
-                    <a
-                      className={cx('nav-link', {
-                        active: item.id === activeSection,
-                      })}
-                      href={`#${item.id}`}
-                      onClick={handleClickAnchor(item.id)}
-                    >
-                      <span>{item.title}</span>
-                    </a>
-                  </li>
-                ))}
-              </ul>
+        <div className="menu-wrapper">
+          <div className="link-list-wrapper menu-link-list">
+            <div className="accordion-wrapper">
+              <Accordion>
+                <AccordionHeader
+                  active={isNavOpen}
+                  onToggle={(a, b) => {
+                    isNavOpen ? setIsNavOpen(false) : setIsNavOpen(true);
+                  }}
+                >
+                  <h3>{intl.formatMessage(messages.index)}</h3>
+                </AccordionHeader>
+                <div className="mb-3">
+                  <Progress value={100 * progressValue} role="progressbar" />
+                </div>
+                <AccordionBody
+                  active={isNavOpen}
+                  className={
+                    isNavOpen
+                      ? 'accordion-collapse show'
+                      : 'accordion-collapse collapse'
+                  }
+                >
+                  <ul className="link-list" data-element="page-index">
+                    {headers.map((item, i) => {
+                      return (
+                        <li className="nav-item" key={item.id}>
+                          <a
+                            className={cx('nav-link', {
+                              active: item.id === activeSection,
+                            })}
+                            href={`#${item.id}`}
+                            onClick={handleClickAnchor(item.id)}
+                          >
+                            <span>{item.title}</span>
+                          </a>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </AccordionBody>
+              </Accordion>
             </div>
           </div>
         </div>
