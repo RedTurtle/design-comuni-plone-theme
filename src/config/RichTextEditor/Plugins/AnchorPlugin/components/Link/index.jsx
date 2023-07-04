26f054e763af762eb9b7a5a734209245ea3f5c71
Martina Bustacchini <martina.bustacchini@redturtle.it>
2023-07-04 16:34:11 +0200

    wip: initial

--- src/config/RichTextEditor/Plugins/AnchorPlugin/components/Link/index.jsx
+++ src/config/RichTextEditor/Plugins/AnchorPlugin/components/Link/index.jsx
@@ -19,7 +19,6 @@ const Link = ({ children, className, entityKey, getEditorState, target }) => {
   const entity = getEditorState().getCurrentContent().getEntity(entityKey);
   const entityData = entity ? entity.get('data') : undefined;
   const href = (entityData && entityData.url) || undefined;
-
   return (
     <a
       className={className}
@@ -30,6 +29,7 @@ const Link = ({ children, className, entityKey, getEditorState, target }) => {
       data-element={entityData.dataElement || entityData['data-element']}
     >
       {children}
+      {!isInternalURL(href) && <i className="external-link-icon"></i>}
     </a>
   );
 };
