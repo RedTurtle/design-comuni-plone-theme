476f952eb71e9ce0c46aa523fd0ab5c9324f0ee2
Giulia Ghisini <giulia.ghisini@redturtle.it>
2023-07-27 12:05:56 +0200

    fix: fix LinkEntity error on create link in WysWidget and in edit

--- src/config/RichTextEditor/LinkEntity.jsx
+++ src/config/RichTextEditor/LinkEntity.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import UniversalLink from '@plone/volto/components/manage/UniversalLink/UniversalLink';
+import { IntlProvider } from 'react-intl';
 
 const LinkEntity = connect((state) => ({
   token: state.userSession.token,
@@ -8,15 +9,18 @@ const LinkEntity = connect((state) => ({
   const { token, url, target, targetUrl, download, children, dataElement } =
     props;
   const to = token ? url : targetUrl || url;
+
   return (
-    <UniversalLink
-      href={to}
-      openLinkInNewTab={target === '_blank' || undefined}
-      download={download}
-      data-element={dataElement || props['data-element'] || null}
-    >
-      {children}
-    </UniversalLink>
+    <IntlProvider>
+      <UniversalLink
+        href={to}
+        openLinkInNewTab={target === '_blank' || undefined}
+        download={download}
+        data-element={dataElement || props['data-element'] || null}
+      >
+        {children}
+      </UniversalLink>
+    </IntlProvider>
   );
 });
 
