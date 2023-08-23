be3250e48506181bede7235553505acfd37c6963
Martina Bustacchini <martina.bustacchini@redturtle.it>
2023-08-23 10:20:12 +0200

    fix:  audit requested changes

--- src/components/ItaliaTheme/Blocks/Listing/Commons/utils.js
+++ src/components/ItaliaTheme/Blocks/Listing/Commons/utils.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 export const getCategory = (item, show_type, show_section, props) => {
   let cat = [];
@@ -17,7 +17,6 @@ export const getCategory = (item, show_type, show_section, props) => {
   }
   return null;
 };
-
 export const visibleSlideTitle = (selector) => {
   // Needed to deal with react-slick duplicating a lot of slides
   // when used in infinite mode. It's an useless and counterproductive
@@ -31,9 +30,34 @@ export const visibleSlideTitle = (selector) => {
 
 export const useSlider = (userAutoplay) => {
   const slider = useRef(null);
+  const onIntersection = (entries, opt) => {
+    entries.forEach((entry) =>
+      entry.target.classList.toggle('visible', entry.isIntersecting),
+    );
+  };
+  const observer = new IntersectionObserver(onIntersection, {
+    root: null,
+    threshold: 0.5,
+  });
+  if (document.querySelector('.block.listing.slider'))
+    observer.observe(document.querySelector('.block.listing.slider'));
+  useEffect(() => {
+    return () => observer.disconnect();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const focusNext = (currentSlide) => {
     const sliderElement = document.querySelector('.block.listing.slider');
+    if (!sliderElement) return;
+    const sliderIsVisible = sliderElement.classList.contains('visible');
+
+    if (!sliderIsVisible && userAutoplay) {
+      slider.current.slickPause();
+      return;
+    }
+    if (sliderIsVisible && userAutoplay) {
+      slider.current.slickPlay();
+    }
     const slide = sliderElement.querySelectorAll(
       `a.slide-link[data-slide="${currentSlide}"]`,
     );
@@ -44,7 +68,9 @@ export const useSlider = (userAutoplay) => {
     const link = visibleSlideTitle(
       `a.slide-link[data-slide="${currentSlide}"]`,
     );
+
     if (!link || document.activeElement === link) {
+      return;
     }
     // eslint-disable-next-line no-unused-expressions
     else link.focus();
