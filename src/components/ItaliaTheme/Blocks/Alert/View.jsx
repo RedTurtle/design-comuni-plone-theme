fded0955c50ce74c3828fadee17441168ef82559
Sabrina Bongiovanni <sabrina.bongiovanni@redturtle.it>
2023-09-20 16:05:06 +0200

    fix: fixed classes, margins and paddings of alertBlock to align text to other elements

--- src/components/ItaliaTheme/Blocks/Alert/View.jsx
+++ src/components/ItaliaTheme/Blocks/Alert/View.jsx
@@ -29,9 +29,9 @@ const View = ({ data, pathname }) => {
 
   return (
     <section role="alert" className="block alertblock">
-      <Row className={cx('row-full-width', 'bg-alert-' + data.color)}>
+      <div className={cx('full-width', 'bg-alert-' + data.color)}>
         <Container className="p-4 pt-5 pb-5">
-          <Row className="align-items-start">
+          <Row className="align-items-start negative-margin">
             {data.image?.data && (
               <Col sm={2} className="pb-3 image-col">
                 <img
@@ -46,7 +46,7 @@ const View = ({ data, pathname }) => {
             <Col>{content}</Col>
           </Row>
         </Container>
-      </Row>
+      </div>
     </section>
   );
 };
