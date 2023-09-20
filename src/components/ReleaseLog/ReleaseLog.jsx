8da7563127597b97b9d6604c10bb010222243c44
Wagner Trezub <wagner.trezub@redturtle.it>
2023-09-20 10:46:14 +0200

    chore: h1 on title

--- src/components/ReleaseLog/ReleaseLog.jsx
+++ src/components/ReleaseLog/ReleaseLog.jsx
@@ -29,9 +29,9 @@ const ReleaseLog = () => {
         <meta name="robots" content="noindex" />
       </Helmet>
       <Container className="px-4 my-4">
-        <h2 className="py-4">
+        <h1 className="py-4">
           Release Log - Lista degli aggiornamenti io-Comune v7
-        </h2>
+        </h1>
         <div dangerouslySetInnerHTML={{ __html: releaseFileContent }}></div>
       </Container>
     </div>
