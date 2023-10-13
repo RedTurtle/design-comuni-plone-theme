611ac6de33083e50c28a18cfdff17663301846b5
Sabrina Bongiovanni <sabrina.bongiovanni@redturtle.it>
2023-10-13 14:55:59 +0200

    fix: removed maps block from config

--- src/config/italiaConfig.js
+++ src/config/italiaConfig.js
@@ -280,9 +280,7 @@ export default function applyConfig(voltoConfig) {
     maxFileUploadSize: null,
     'volto-blocks-widget': {
       allowedBlocks: [
-        ...(config.settings['volto-blocks-widget']?.allowedBlocks ?? []).filter(
-          (block) => block.id !== 'maps',
-        ),
+        ...(config.settings['volto-blocks-widget']?.allowedBlocks ?? []),
         'break',
         'testo_riquadro_semplice',
         'testo_riquadro_immagine',
@@ -410,10 +408,10 @@ export default function applyConfig(voltoConfig) {
       ...config.blocks.blocksConfig.slateTable,
       restricted: true,
     },
-    maps: {
-      ...config.blocks.blocksConfig.maps,
-      restricted: true,
-    },
+    // maps: {
+    //   ...config.blocks.blocksConfig.maps,
+    //   restricted: true,
+    // },
     search: {
       ...config.blocks.blocksConfig.search,
       icon: searchIcon,
@@ -452,6 +450,10 @@ export default function applyConfig(voltoConfig) {
   // We chose to disallow leadimage block usage in editor. If you want it back someday,
   // comment out the following line and add the leadimage behavior in Document.xml file
   delete config.blocks.blocksConfig['leadimage'];
+
+  // BUG#41117
+  // Removed Maps block for privacy guidelines
+  delete config.blocks.blocksConfig.maps;
 
   // TOC block anchors not working, customizing tocEntry
   // to also return draftJS block id
