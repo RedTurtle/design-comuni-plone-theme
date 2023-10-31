a5cca3985534a4a386196a74c1c59d047dea3f8a
Mauro Amico <mauro.amico@gmail.com>
2023-10-31 15:33:27 +0100

    fix: change image component subevents

--- src/components/ItaliaTheme/View/Commons/Events.jsx
+++ src/components/ItaliaTheme/View/Commons/Events.jsx
@@ -1,3 +1,6 @@
+/*
+ * TODO: ....
+ */
 import PropTypes from 'prop-types';
 import { defineMessages, useIntl } from 'react-intl';
 import {
@@ -7,11 +10,9 @@ import {
   CardText,
   CardTitle,
 } from 'design-react-kit';
-
+import config from '@plone/volto/registry';
 import { UniversalLink } from '@plone/volto/components';
 import { flattenToAppURL } from '@plone/volto/helpers';
-
-import Image from '@plone/volto/components/theme/Image/Image';
 import { viewDate } from 'design-comuni-plone-theme/helpers';
 
 const messages = defineMessages({
@@ -37,7 +38,7 @@ const messages = defineMessages({
  */
 const Evento = ({ event, show_image }) => {
   const intl = useIntl();
-
+  const Image = config.getComponent({ name: 'Image' }).component;
   return event ? (
     <div className="card-wrapper card-teaser">
       <Card noWrapper className="card-img no-after">
@@ -45,17 +46,14 @@ const Evento = ({ event, show_image }) => {
           (event.image_field || event.preview_image || event.image) && (
             <div className="img-responsive-wrapper">
               <div className="img-responsive">
-                <Image
-                  itemUrl={event.image_field ? event['@id'] : undefined}
-                  image={
-                    event.image_scales?.[event.image_field]?.[0] ||
-                    event.preview_image ||
-                    event.image ||
-                    event['@id']
-                  }
-                  alt={intl.formatMessage(messages.immagine)}
-                  containerClassName="img-wrapper"
-                />
+                <figure class="volto-image img-wrapper responsive">
+                  <Image
+                    item={event}
+                    alt={intl.formatMessage(messages.immagine)}
+                    loading="lazy"
+                    sizes="(max-width:320px) 300px, (max-width:425px) 400px, (max-width:768px) 600px, 300px"
+                  />
+                </figure>
                 {event.start && (
                   <div className="card-calendar d-flex flex-column justify-content-center">
                     {viewDate(intl.locale, event.start, 'DD MMM')}
@@ -95,7 +93,6 @@ const Evento = ({ event, show_image }) => {
  */
 const Events = ({ content, title, show_image, folder_name, isChild }) => {
   const intl = useIntl();
-
   const events = isChild
     ? content?.parent
       ? [content.parent]
