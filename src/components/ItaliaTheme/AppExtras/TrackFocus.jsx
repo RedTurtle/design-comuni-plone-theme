02df64462d724e10f3bee0cc92afa55e5d1db058
Wagner Trezub <wagner.trezub@redturtle.it>
2024-03-04 16:29:21 +0100

    fix: active class

--- src/components/ItaliaTheme/AppExtras/TrackFocus.jsx
+++ src/components/ItaliaTheme/AppExtras/TrackFocus.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 
 const DATA_FOCUS_MOUSE = 'data-focus-mouse';
 const CLASS_NAME_MOUSE_FOCUS = 'focus--mouse';
+const CLASS_NAME_MOUSE_ACTIVE = 'active';
 
 const TrackFocus = () => {
   const [usingMouse, setUsingMouse] = useState(false);
 
   useEffect(() => {
     const handleEvent = (e) => {
-      setUsingMouse(e.type === 'mousedown');
+      setUsingMouse(e.type === 'mousedown' ? true : false);
     };
 
     document.addEventListener('keydown', handleEvent);
@@ -25,20 +26,30 @@ const TrackFocus = () => {
       if (e.target) {
         if (usingMouse) {
           e.target.classList.add(CLASS_NAME_MOUSE_FOCUS);
+          e.target.classList.add(CLASS_NAME_MOUSE_ACTIVE);
           e.target.setAttribute(DATA_FOCUS_MOUSE, true);
         } else {
           e.target.classList.remove(CLASS_NAME_MOUSE_FOCUS);
+          e.target.classList.remove(CLASS_NAME_MOUSE_ACTIVE);
           e.target.setAttribute(DATA_FOCUS_MOUSE, false);
         }
       }
     };
 
+    const handleFocusOut = (e) => {
+      if (e.target) {
+        e.target.classList.remove(CLASS_NAME_MOUSE_FOCUS);
+        e.target.classList.remove(CLASS_NAME_MOUSE_ACTIVE);
+        e.target.setAttribute(DATA_FOCUS_MOUSE, false);
+      }
+    };
+
     document.addEventListener('focusin', handleFocusChange);
-    document.addEventListener('focusout', handleFocusChange);
+    document.addEventListener('focusout', handleFocusOut);
 
     return () => {
       document.removeEventListener('focusin', handleFocusChange);
-      document.removeEventListener('focusout', handleFocusChange);
+      document.removeEventListener('focusout', handleFocusOut);
     };
   }, [usingMouse]);
 
