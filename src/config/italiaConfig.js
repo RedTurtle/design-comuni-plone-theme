f8ab584e1e7a57f8630a85daa5a1ddbad1f31d90
Sabrina Bongiovanni <sabrina.bongiovanni@redturtle.it>
2024-04-17 10:03:42 +0200

    fix: added parameter to show footer links marker

--- src/config/italiaConfig.js
+++ src/config/italiaConfig.js
@@ -66,8 +66,8 @@ import { schemaListing } from 'design-comuni-plone-theme/components/ItaliaTheme/
 
 import reducers from 'design-comuni-plone-theme/reducers';
 
-const ReleaseLog = loadable(
-  () => import('design-comuni-plone-theme/components/ReleaseLog/ReleaseLog'),
+const ReleaseLog = loadable(() =>
+  import('design-comuni-plone-theme/components/ReleaseLog/ReleaseLog'),
 );
 
 const messages = defineMessages({
@@ -270,6 +270,7 @@ export default function applyConfig(voltoConfig) {
       splitMegamenuColumns: true, //se impostato a false, non spezza le colonne con intestazioni nel megamenu
       footerNavigationDepth: 2, //valori possibili: [1,2]. Se impostato ad 1 non verranno mostrati nel footer i link agli elementi contenuti nelle sezioni di primo livello.
       markSpecialLinks: true, // se impostato a false, non marca con icona i link esterni
+      markFooterLinks: false,
     },
     apiExpanders: [
       ...config.settings.apiExpanders,
