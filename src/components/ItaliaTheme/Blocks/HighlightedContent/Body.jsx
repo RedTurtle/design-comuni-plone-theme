d34175b0068d7c000b7be91b8609486409f66519
Giulia Ghisini <giulia.ghisini@redturtle.it>
2024-04-18 10:30:57 +0200

    fix: remove unnecessary fetchpriority

--- src/components/ItaliaTheme/Blocks/HighlightedContent/Body.jsx
+++ src/components/ItaliaTheme/Blocks/HighlightedContent/Body.jsx
@@ -56,7 +56,6 @@ const Body = (props) => {
               role={null}
               responsive={true}
               sizes="(max-width:768px) 90vw, (max-width:1024px) 450px, 560px"
-              fetchpriority="high"
             />
           </Col>
         )}
