0b2d71d7699be7fa1f8bf21ac575b354a2f2dda9
Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>
2024-07-12 12:31:39 +0200

    fix: view dates for Events in Listing image template (#717)

--- src/components/ItaliaTheme/Blocks/Listing/Commons/CardCalendar.jsx
+++ src/components/ItaliaTheme/Blocks/Listing/Commons/CardCalendar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useIntl, defineMessages } from 'react-intl';
-
+import cx from 'classnames';
 import {
   viewDate,
   getRealStartAndEndWithRecurrence,
@@ -34,32 +34,102 @@ export const CardCalendar = ({ start, end, recurrence, rrule }) => {
     intl,
   );
   if ((_start && _end) || recurrenceDates) {
-    if (!recurrenceDates && _start.isSame(_end, 'day')) {
+    if (!recurrenceDates) {
+      if (_start.isSame(_end, 'day')) {
+        return (
+          <div className="card-calendar d-flex flex-column justify-content-center">
+            <span className="card-date date">{_start.format('D')}</span>
+            <span className="card-day date">{_end.format('MMMM')}</span>
+          </div>
+        );
+      } else if (!_start.isSame(_end, 'year')) {
+        return (
+          <div className="custom-calendar-card">
+            <div className="card-calendar extended d-flex flex-column justify-content-center px-2 w-auto">
+              <span className="card-date d-flex justify-content-between align-items-baseline">
+                <div className="date-label me-1">
+                  {intl.formatMessage(messages.from)}
+                </div>
+                <span className="date">{_start.format('DD/MM/YY')}</span>
+              </span>
+              <span className="card-date d-flex justify-content-between align-items-baseline">
+                <div className="date-label me-1">
+                  {intl.formatMessage(messages.to)}
+                </div>
+                <span className="date">{_end.format('DD/MM/YY')}</span>
+              </span>
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="custom-calendar-card">
+            <div className="card-calendar extended d-flex flex-column justify-content-center px-2">
+              <span className="card-date d-flex justify-content-between align-items-baseline">
+                <div className="date-label me-1">
+                  {intl.formatMessage(messages.from)}
+                </div>
+                <span className="date">{_start.format('DD/MM')}</span>
+              </span>
+              <span className="card-date d-flex justify-content-between align-items-baseline">
+                <div className="date-label me-1">
+                  {intl.formatMessage(messages.to)}
+                </div>
+                <span className="date">{_end.format('DD/MM')}</span>
+              </span>
+            </div>
+          </div>
+        );
+      }
+    } else if (recurrenceDates) {
+      let dateFormat = 'DD/MM';
+      if (
+        !recurrenceDates?.recurrenceStart?.isSame(
+          recurrenceDates?.recurrenceEnd,
+          'year',
+        )
+      )
+        dateFormat = 'DD/MM/YY';
       return (
-        <div className="card-calendar d-flex flex-column justify-content-center">
-          <span className="card-date">{_start.format('D')}</span>
-          <span className="card-day">{_end.format('MMMM')}</span>
+        <div className="custom-calendar-card">
+          <div
+            className={cx(
+              'card-calendar extended d-flex flex-column justify-content-center px-2',
+              {
+                'w-auto': dateFormat !== 'DD/MM',
+              },
+            )}
+          >
+            <span className="card-date d-flex justify-content-between align-items-baseline">
+              <div className="date-label me-1">
+                {intl.formatMessage(messages.from)}
+              </div>
+              <span className="date">
+                {recurrenceDates.recurrenceStart?.format(dateFormat)}
+              </span>
+            </span>
+            <span className="card-date d-flex justify-content-between align-items-baseline">
+              <div className="date-label me-1">
+                {intl.formatMessage(messages.to)}
+              </div>
+              <span className="date">
+                {recurrenceDates?.recurrenceEnd?.format(dateFormat)}
+              </span>
+            </span>
+          </div>
         </div>
       );
-    } else if (recurrenceDates) {
+    } else if (_start.format('M') === _end.format('M')) {
       return (
         <div className="custom-calendar-card">
           <div className="card-calendar d-flex flex-column justify-content-center">
             <span className="card-date d-flex justify-content-center align-items-baseline">
-              <div className="date-label mr-1">
-                {intl.formatMessage(messages.from)}
-              </div>
-              <span className="date">
-                {recurrenceDates.recurrenceStart?.format('DD/MM')}
-              </span>
+              <span className="date day start-date">{_start.format('D')}</span>{' '}
+              <span className="date day divider">-</span>{' '}
+              <span className="date day end-date">{_end.format('D')}</span>
             </span>
             <span className="card-date d-flex justify-content-center align-items-baseline">
-              <div className="date-label mr-1">
-                {intl.formatMessage(messages.to)}
-              </div>
-              <span className="date">
-                {recurrenceDates?.recurrenceEnd?.format('DD/MM')}
-              </span>
+              <span className="date month">{_end.format('MMM')}</span>
             </span>
           </div>
         </div>
@@ -67,15 +137,15 @@ export const CardCalendar = ({ start, end, recurrence, rrule }) => {
     } else {
       return (
         <div className="custom-calendar-card">
-          <div className="card-calendar d-flex flex-column justify-content-center">
-            <span className="card-date d-flex justify-content-center align-items-baseline">
-              <div className="date-label mr-1">
+          <div className="card-calendar extended d-flex flex-column justify-content-center px-2">
+            <span className="card-date d-flex justify-content-between align-items-baseline">
+              <div className="date-label me-1">
                 {intl.formatMessage(messages.from)}
               </div>
               <span className="date">{_start.format('DD/MM')}</span>
             </span>
-            <span className="card-date d-flex justify-content-center align-items-baseline">
-              <div className="date-label mr-1">
+            <span className="card-date d-flex justify-content-between align-items-baseline">
+              <div className="date-label me-1">
                 {intl.formatMessage(messages.to)}
               </div>
               <span className="date">{_end.format('DD/MM')}</span>
