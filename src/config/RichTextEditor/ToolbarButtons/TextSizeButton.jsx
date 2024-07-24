a5ae851994a3249c267fac5677449343da0c4071
Sara Bianchi <sara.bianchi@redturtle.it>
2024-07-24 10:55:18 +0200

    fix: fixed list style format in sidebar context and TextSize icon

--- src/config/RichTextEditor/ToolbarButtons/TextSizeButton.jsx
+++ src/config/RichTextEditor/ToolbarButtons/TextSizeButton.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { defineMessages, useIntl } from 'react-intl';
+// import { defineMessages, useIntl } from 'react-intl';
 import Icon from '@plone/volto/components/theme/Icon/Icon';
 
 // import DraftJsDropdownButton from 'design-comuni-plone-theme/config/RichTextEditor/ToolbarButtons/DraftJsDropdownButton';
 
 import formatSVG from '@plone/volto/icons/format.svg';
 
-const messages = defineMessages({
-  TextSizeButton: {
-    id: 'text-size-button',
-    defaultMessage: 'Dimensione del testo',
-  },
-});
+// const messages = defineMessages({
+//   TextSizeButton: {
+//     id: 'text-size-button',
+//     defaultMessage: 'Dimensione del testo',
+//   },
+// });
 
 /*
 const TextSizeButton = (props) => {
@@ -44,22 +44,12 @@ const TextSizeButton = (props) => {
 };
 */
 
-const ButtonIcon = (props) => {
-  const intl = useIntl();
-  return (
-    <Icon
-      name={formatSVG}
-      size="1.25em"
-      title={intl.formatMessage(messages.TextSizeButton)}
-    />
-  );
-};
-
 const TextSizeButton = (props) => {
   const createInlineStyleButton = props.draftJsCreateInlineStyleButton.default;
+
   return createInlineStyleButton({
     style: 'TEXT_LARGER',
-    children: ButtonIcon,
+    children: <Icon name={formatSVG} size="24px" />,
   });
 };
 export default TextSizeButton;
