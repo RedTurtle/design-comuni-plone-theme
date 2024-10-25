d96b8886d6b839b3c802afeac921b1ed5f72937f
Wagner Trezub <wagner.trezub@redturtle.it>
2024-10-25 12:03:02 +0200

    fix: condition with has no content

--- src/components/ItaliaTheme/Blocks/Alert/View.jsx
+++ src/components/ItaliaTheme/Blocks/Alert/View.jsx
@@ -27,7 +27,7 @@ const View = ({ data, pathname }) => {
       )
     : '';
 
-  return (
+  return content ? (
     <section role="alert" className="block alertblock">
       <Row className={cx('row-full-width', 'bg-alert-' + data.color)}>
         <Container className="p-4 pt-5 pb-5">
@@ -50,6 +50,8 @@ const View = ({ data, pathname }) => {
         </Container>
       </Row>
     </section>
+  ) : (
+    <></>
   );
 };
 
