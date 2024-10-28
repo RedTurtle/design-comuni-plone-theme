558c33f2995193856b8b6bb8ba9261fb35e76311
Wagner Trezub <60133113+Wagner3UB@users.noreply.github.com>
2024-10-28 14:11:24 +0100

    Update src/components/ItaliaTheme/Blocks/Alert/Sidebar.jsx
    Co-authored-by: Giulia Ghisini <51911425+giuliaghisini@users.noreply.github.com>

--- src/components/ItaliaTheme/Blocks/Alert/Sidebar.jsx
+++ src/components/ItaliaTheme/Blocks/Alert/Sidebar.jsx
@@ -37,7 +37,7 @@ const messages = defineMessages({
     defaultMessage: 'Inizio',
   },
   endDate: {
-    id: 'endDate',
+    id: 'alertblock_endDate',
     defaultMessage: 'Scadenza',
   },
   dateTitle: {
