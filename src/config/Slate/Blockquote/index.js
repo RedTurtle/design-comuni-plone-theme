632895f87e830a134c54a96edc3edb1c38fca251
Giulia Ghisini <giulia.ghisini@redturtle.it>
2025-03-07 11:01:18 +0100

    fix: typo

--- src/config/Slate/Blockquote/index.js
+++ src/config/Slate/Blockquote/index.js
@@ -4,14 +4,14 @@ import BlockquoteMenu from './BlockquoteMenu';
 export default function install(config) {
   const { slate } = config.settings;
 
-  (slate.elements.blockquote = ({ children }) => (
+  slate.elements.blockquote = ({ children }) => (
     <blockquote>
       <p>{children}</p>
     </blockquote>
-  )),
-    (slate.buttons.blockquote = (props) => (
-      <BlockquoteMenu {...props} title="Blockquote" />
-    ));
+  );
+  slate.buttons.blockquote = (props) => (
+    <BlockquoteMenu {...props} title="Blockquote" />
+  );
 
   return config;
 }
