533c60559aaa4d6cbfe829db79b5c9c1bfda7b36
sabrina.bongiovanni <sabrina.bongiovanni@redturtle.it>
2025-03-11 17:30:08 +0100

    fix: added label to TextFilter component

--- src/components/ItaliaTheme/Blocks/Common/SearchFilters/TextFilter.jsx
+++ src/components/ItaliaTheme/Blocks/Common/SearchFilters/TextFilter.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useIntl, defineMessages } from 'react-intl';
+import { v4 as uuid } from 'uuid';
 
 const messages = defineMessages({
   text_filter_placeholder: {
@@ -10,8 +11,12 @@ const messages = defineMessages({
 
 const TextFilter = ({ value, id, onChange, placeholder }) => {
   const intl = useIntl();
+  const filterID = uuid();
   return (
     <div className="me-lg-3 my-2 my-lg-1 filter-wrapper text-filter">
+      <label for={filterID} className="visually-hidden">
+        {placeholder}
+      </label>
       <input
         type="text"
         placeholder={
@@ -22,6 +27,7 @@ const TextFilter = ({ value, id, onChange, placeholder }) => {
           onChange(id, e.target.value ?? '');
         }}
         autocomplete="off"
+        id={filterID}
       />
     </div>
   );
