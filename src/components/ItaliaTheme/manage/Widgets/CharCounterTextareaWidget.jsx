66cd7d6055f98e869c185ed727ebae6db615c969
Mauro Amico <mauro.amico@gmail.com>
2025-06-03 10:17:33 +0200

    fix: fallback

--- src/components/ItaliaTheme/manage/Widgets/CharCounterTextareaWidget.jsx
+++ src/components/ItaliaTheme/manage/Widgets/CharCounterTextareaWidget.jsx
@@ -34,7 +34,7 @@ const messages = defineMessages({
 const CharCounterTextareaWidget = (props) => {
   const { id, value, onChange, placeholder, intl } = props;
   const maxLength = Math.max(props.maxLength || 255, 1);
-  const forceMaxLength = props.forceMaxLength || true;
+  const forceMaxLength = props.forceMaxLength ?? true;
   const [textChar, setTextChar] = useState('');
 
   const lengthError = value?.length > 0 && value.length > maxLength;
