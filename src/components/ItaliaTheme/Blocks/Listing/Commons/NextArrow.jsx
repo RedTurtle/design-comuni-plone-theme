5ad4cd1cc98c4a044754dbb8d65c55f98e098868
Wagner Trezub <wagner.trezub@redturtle.it>
2026-04-21 16:58:19 +0200

    fix: PrevArrow and NextArrow

--- src/components/ItaliaTheme/Blocks/Listing/Commons/NextArrow.jsx
+++ src/components/ItaliaTheme/Blocks/Listing/Commons/NextArrow.jsx
@@ -4,7 +4,7 @@ export default function NextArrow(props) {
   const { className, style, onClick } = props;
   return (
     <div className={className} style={{ ...style }} onClick={onClick}>
-      <Icon icon="chevron-right" key="chevron-right" />
+      <Icon icon="chevron-right" key="chevron-right" aria-hidden={true} />
     </div>
   );
 }
