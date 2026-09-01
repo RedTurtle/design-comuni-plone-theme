2b0e667273e34c2ff944960a769e5bec7b29e653
Giulia Ghisini <giulia.ghisini@redturtle.it>
2026-09-01 11:03:00 +0200

    fix: icon blocks bg image vs color

--- src/components/ItaliaTheme/Blocks/IconBlocks/View.jsx
+++ src/components/ItaliaTheme/Blocks/IconBlocks/View.jsx
@@ -6,11 +6,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ViewBlock from './Block/ViewBlock';
-import { Container, Row, Col } from 'design-react-kit';
+import { Row, Col } from 'design-react-kit';
 import { flattenToAppURL } from '@plone/volto/helpers';
 import { UniversalLink } from '@plone/volto/components';
 import { TextBlockView } from '@plone/volto-slate/blocks/Text';
-import config from '@plone/volto/registry';
+import Wrapper from './Wrapper';
 
 /**
  * View IconsBlock block class.
@@ -19,70 +19,48 @@ import config from '@plone/volto/registry';
  */
 const IconsBlockView = ({ data, block }) => {
   const id = new Date().getTime();
-  const Image = config.getComponent({ name: 'Image' }).component;
   const xlColumns = `${12 / (data.columns ?? 4)}`;
 
   return (
     <div className="block iconBlocks">
-      <div className="public-ui">
-        <div
-          className={`full-width section py-5 ${
-            data.bg_color === 'none' ? '' : 'bg-' + (data.bg_color ?? 'primary')
-          }`}
-        >
-          {data.background?.[0] ? (
-            <div className="background-image">
-              <Image
-                item={data.background[0]}
-                alt=""
-                role={null}
-                responsive={true}
-                sizes="100vw"
-              />
+      <Wrapper data={data}>
+        <div className="block-header">
+          {data.title && (
+            <div className="title">
+              <h2>{data.title}</h2>
             </div>
-          ) : (
-            <div className="background-image no-image"></div>
           )}
-          <Container className="px-md-4">
-            <div className="block-header">
-              {data.title && (
-                <div className="title">
-                  <h2>{data.title}</h2>
-                </div>
-              )}
-              {data.description && (
-                <div className="description">
-                  <TextBlockView data={{ value: data.description }} />
-                </div>
-              )}
+          {data.description && (
+            <div className="description">
+              <TextBlockView data={{ value: data.description }} />
             </div>
-            <Row>
-              {data.subblocks.map((subblock, index) => (
-                <Col lg="4" xl={xlColumns} key={subblock.id}>
-                  <ViewBlock
-                    data={subblock}
-                    key={index}
-                    id={id}
-                    index={index}
-                    blockHasTitle={!!data.title}
-                  />
-                </Col>
-              ))}
-            </Row>
-
-            {data.href && data.linkMoreTitle && (
-              <div className="link-button text-center my-4">
-                <UniversalLink
-                  href={flattenToAppURL(data.href)}
-                  className="btn btn-tertiary"
-                >
-                  {data.linkMoreTitle}
-                </UniversalLink>
-              </div>
-            )}
-          </Container>
+          )}
         </div>
-      </div>
+        <Row>
+          {data.subblocks.map((subblock, index) => (
+            <Col lg="4" xl={xlColumns} key={subblock.id}>
+              <ViewBlock
+                data={subblock}
+                key={index}
+                id={id}
+                index={index}
+                blockHasTitle={!!data.title}
+              />
+            </Col>
+          ))}
+        </Row>
+
+        {data.href && data.linkMoreTitle && (
+          <div className="link-button text-center my-4">
+            <UniversalLink
+              href={flattenToAppURL(data.href)}
+              className="btn btn-tertiary"
+            >
+              {data.linkMoreTitle}
+            </UniversalLink>
+          </div>
+        )}
+      </Wrapper>
     </div>
   );
 };
